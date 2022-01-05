2e99545b685d6fe5441c4331d531ffee9b5d7a90
Johnny Lay <johnny.lay.developer@gmail.com>
2022-01-05 12:50:29 +0100

    Setting up firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// initialize firebase
+const app = initializeApp(firebaseConfig);
+
+// firebase authentication instance
+const auth = getAuth();
+
+// firebase firestore instance
+const db = getFirestore(app);
+
+// firebase storage instance
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
