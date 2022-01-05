976d54edf181d7f595304a57b172361cc637a36e
Johnny Lay <johnny.lay.developer@gmail.com>
2022-01-05 18:02:06 +0100

    Added welcome message for logged in user

---
+++ src/components/home/WelcomeMessageUser.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+const WelcomeMessageUser = () => {
+  return (
+    <>
+      <h2 className="text-center text-light py-3 fw-bold">
+        Photo Reviewer Alpha
+      </h2>
+      <p className="text-center text-light">Let's get started!</p>
+      <p className="text-center text-light">
+        Head over to{" "}
+        <Link className="text-light" to="/albums">
+          Albums
+        </Link>{" "}
+        to create your album, upload your photos and then send your review link
+        to your customer.
+      </p>
+    </>
+  );
+};
+
+export default WelcomeMessageUser;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,14 @@
 import Container from "react-bootstrap/Container";
 import WelcomeMessageGuest from "../components/home/WelcomeMessageGuest";
+import WelcomeMessageUser from "../components/home/WelcomeMessageUser";
+import useAuthContext from "../hooks/useAuthContext";
 
 const HomePage = () => {
+  const { currentUser } = useAuthContext();
+
   return (
     <Container className="my-5">
-      <WelcomeMessageGuest />
+      {!currentUser ? <WelcomeMessageGuest /> : <WelcomeMessageUser />}
     </Container>
   );
 };
