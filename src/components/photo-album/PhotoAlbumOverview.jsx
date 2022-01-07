4869e79f5cae217e6f1a060425b914332b500dd0
Johnny Lay <johnny.lay.developer@gmail.com>
2022-01-07 14:12:01 +0100

    Added a "Go back" button for Photo Album's page

--- src/components/photo-album/PhotoAlbumOverview.jsx
+++ src/components/photo-album/PhotoAlbumOverview.jsx
@@ -1,11 +1,27 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import Button from "react-bootstrap/Button";
 import PhotoAlbumHeader from "./PhotoAlbumHeader";
 
 const PhotoAlbumOverview = () => {
   const { albumId } = useParams();
 
+  const navigate = useNavigate();
+
   return (
     <>
+      <Button
+        onClick={() => navigate("/photo-albums")}
+        className="mb-5"
+        variant="dark"
+      >
+        <span aria-label="an arrow left icon" className="me-1">
+          <FontAwesomeIcon icon={faArrowCircleLeft} size="1x" color="white" />
+        </span>{" "}
+        Go back
+      </Button>
+
       <PhotoAlbumHeader albumId={albumId} />
 
       <hr className="bg-light my-5" />
